42483c5374bb7d03b73adae6f6a9d85722712072
Jhom <jhomwan238@gmail.com>
2022-06-28 00:10:31 +0700

    Add javascript to make clock functions

--- time-clock/script.js
+++ time-clock/script.js
@@ -37,7 +37,28 @@ function setTime() {
   const time = new Date();
   const month = time.getMonth();
   const day = time.getDay();
-  const hour = time.getHours() % 12;
+  const date = time.getDate();
+  const hour_12 = time.getHours() % 12;
   const minute = time.getMinutes();
   const second = time.getSeconds();
+
+  //Rotate Clock's needles
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hour_12, 0, 11, 0, 360)}deg)`
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minute, 0, 59, 0, 360)}deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(second, 0, 59, 0, 360)}deg)`
+
+  //Display Time 
+  const hour_24 =  time.getHours()
+  timeEl.innerHTML = `${hour_24}:${minute < 10 ? `0${minute}`: minute} ${hour_24 < 12 ? 'AM':'PM'}`
+
+  //Display Date
+  dateEl.innerHTML = `${days[day]}, ${months[month]} ${date}`
 }
+
+//StackOverflow - Map a range of numbers to another range of numbers
+//Map 0 - 11 hours to 0 - 360 degrees
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+setInterval(setTime, 1000)
